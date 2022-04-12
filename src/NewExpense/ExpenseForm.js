e7151a5587ec705d97046f7ff1dfb5ff1c7235a5
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-04-13 02:10:59 +0530

    feat: add two way binding in expense form

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -1,20 +1,53 @@
 import "./ExpenseForm.css";
+import {useState} from 'react';
 
 const ExpenseForm = () => {
+
+  const [enteredTitle, setTitle] = useState('');
+  const [enteredAmount, setAmount] = useState('');
+  const [enteredDate, setDate] = useState('');
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value)
+  }
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+    const expenses = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate
+    }
+    console.log(expenses)
+
+    setTitle(' ');
+    setAmount(' ');
+    setDate(' ');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number"  min='0.01' step='0.01' />
+          <input type="number" value={enteredAmount} onChange={amountChangeHandler} min='0.01' step='0.01' />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min='2019-01-01' max='2022-12-31' />
+          <input type="date" value={enteredDate} onChange={dateChangeHandler} min='2019-01-01' max='2022-12-31' />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
