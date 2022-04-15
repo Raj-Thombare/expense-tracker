0602f81504a5dd0c2546399b7d42f3414c7127e7
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-04-16 01:55:32 +0530

    feat: add year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,40 @@
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2022");
+
   const expensesData = props.expenses;
+
+  const filteredData = expensesData.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  console.log(filteredData);
+
+  const filteredYearHandler = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
     <Card className="expenses">
-      {expensesData.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          key={expense.id}
-        />
-      ))}
+      <ExpenseFilter
+        selected={selectedYear}
+        getFilteredYear={filteredYearHandler}
+      />
+      {
+        filteredData.map((expense) => {
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={expense.id}
+          />;
+        })
+      }
     </Card>
   );
 };
